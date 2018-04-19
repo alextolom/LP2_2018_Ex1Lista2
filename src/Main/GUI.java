cea98fde8fc58a89b686a5c101d73908a271a3f4
a1817744 <a1817744@debian9-dacom.dacom>
2018-04-19 16:28:54 -0300

    prova 1 bim

--- src/Main/GUI.java
+++ src/Main/GUI.java
@@ -1,54 +1,114 @@
 package Main;
 
 //@author Radames J Halmeman  - rjhalmeman@gmail.com
+import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class GUI extends JFrame {
 
     private Container cp;
-    private JLabel lbA = new JLabel("Rótulo A");
-    private JTextField tfA = new JTextField(5);
-
+    
+    private JPanel painelCentro = new JPanel();
+    
+    private JLabel lbCorretor1 = new JLabel("Corretor 1");
+    private JLabel lbCorretor2 = new JLabel("Corretor 2");
+    private JLabel lbCorretor3 = new JLabel("Corretor 3");
+    private JLabel lbComissao1 = new JLabel("Comissão 1");
+    private JLabel lbComissao2 = new JLabel("Comissão 2");
+    private JLabel lbComissao3 = new JLabel("Comissão 3");
+    
+    private JPanel painelCentroLinha1 = new JPanel();
+    private JPanel painelCentroLinha2 = new JPanel();
+    private JPanel painelCentroLinha3 = new JPanel();
+    private JPanel painelCentroLinha4 = new JPanel();
+    private JPanel painelCentroLinha5 = new JPanel();
+    private JPanel painelCentroLinha6 = new JPanel();
+    private JPanel painelCentroLinha7 = new JPanel();
+    
+    private JTextField tfCorretor1 = new JTextField(10);
+    private JTextField tfCorretor2 = new JTextField(10);
+    private JTextField tfCorretor3 = new JTextField(10);
+    private JTextField tfComissao1 = new JTextField(10);
+    private JTextField tfComissao2 = new JTextField(10);
+    private JTextField tfComissao3 = new JTextField(10);
+    
     private JButton btCalcular = new JButton("Calcular");
-
-    private JLabel lbResp = new JLabel("Resp");
-
-    private JTextField tfResp = new JTextField(5);
+    
+    private JLabel lbVazio1 = new JLabel("                           ");
+    private JLabel lbVazio2 = new JLabel("                           ");
+    private JLabel lbVazio3 = new JLabel("                           ");
+    private JLabel lbVazio4 = new JLabel("                           ");
+    private JLabel lbVazio5 = new JLabel("                           ");
+    private JLabel lbVazio6 = new JLabel("                           ");
+    
 
     public GUI() {
-        setSize(800, 600);
+        setSize(600, 400);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         cp = getContentPane();
-        cp.setLayout(new GridLayout(3, 2));
-        setTitle("Básico");
+        cp.setLayout(new BorderLayout());
+        setTitle("Alexandre Tolomeotti - 1 Bimestre");
+        
+        cp.add(painelCentro, BorderLayout.CENTER);
+        painelCentro.setLayout(new GridLayout(7,1));
+        
+        painelCentro.add(painelCentroLinha1);
+        painelCentro.add(painelCentroLinha2);
+        painelCentro.add(painelCentroLinha3);
+        painelCentro.add(painelCentroLinha4);
+        painelCentro.add(painelCentroLinha5);
+        painelCentro.add(painelCentroLinha6);
+        painelCentro.add(painelCentroLinha7);
+        
+        painelCentroLinha1.setLayout(new FlowLayout(FlowLayout.LEFT));
+        painelCentroLinha1.add(lbCorretor1);
+        painelCentroLinha1.add(tfCorretor1);
+        
+        painelCentroLinha2.setLayout(new FlowLayout(FlowLayout.LEFT));
+        painelCentroLinha2.add(lbCorretor2);
+        painelCentroLinha2.add(tfCorretor2);
+        
+        painelCentroLinha3.setLayout(new FlowLayout(FlowLayout.LEFT));
+        painelCentroLinha3.add(lbCorretor3);
+        painelCentroLinha3.add(tfCorretor3);
 
-        cp.add(lbA);
-        cp.add(tfA);
-
-        cp.add(btCalcular);
-        cp.add(new JLabel(""));
-        cp.add(lbResp);
-        cp.add(tfResp);
-
+        painelCentroLinha4.add(btCalcular);
+        
+        painelCentroLinha5.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        painelCentroLinha5.add(lbComissao1);
+        painelCentroLinha5.add(tfComissao1);
+        
+        painelCentroLinha6.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        painelCentroLinha6.add(lbComissao2);
+        painelCentroLinha6.add(tfComissao2);
+        
+        painelCentroLinha7.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        painelCentroLinha7.add(lbComissao3);
+        painelCentroLinha7.add(tfComissao3);
+        
         btCalcular.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Processamento mp = new Processamento();
-                //enviar dados
-                mp.setValor(Double.valueOf(tfA.getText()));
-
-                //obter resposta
-                tfResp.setText(String.valueOf(mp.getResposta()));
-
+                Processamento processamento = new Processamento();
+                processamento.setCorretor1(Double.valueOf(tfCorretor1.getText()));
+                processamento.setCorretor2(Double.valueOf(tfCorretor2.getText()));
+                processamento.setCorretor3(Double.valueOf(tfCorretor3.getText()));
+                
+                tfComissao1.setText(String.valueOf(processamento.getComissao1()));
+                tfComissao2.setText(String.valueOf(processamento.getComissao2()));
+                tfComissao3.setText(String.valueOf(processamento.getComissao3()));
             }
         });
+        
 
         setLocationRelativeTo(null);//centraliza no monitor
         setVisible(true);
--- src/Main/Processamento.java
+++ src/Main/Processamento.java
@@ -3,20 +3,81 @@ package Main;
 //@author Radames J Halmeman  - rjhalmeman@gmail.com
 public class Processamento {
 
-    private double valor;
-    private double resposta;
+    private double corretor1;
+    private double corretor2;
+    private double corretor3;
+    
+    private double comissao1;
+    private double comissao2;
+    private double comissao3;
 
-    public double getValor() {
-        return valor;
+    public double getCorretor1() {
+        return corretor1;
     }
 
-    public void setValor(double valor) {
-        this.valor = valor;
+    public void setCorretor1(double corretor1) {
+        this.corretor1 = corretor1;
     }
 
-    public double getResposta() {
-        resposta = valor*2;
-        return resposta;
+    public double getCorretor2() {
+        return corretor2;
+    }
+
+    public void setCorretor2(double corretor2) {
+        this.corretor2 = corretor2;
+    }
+
+    public double getCorretor3() {
+        return corretor3;
+    }
+
+    public void setCorretor3(double corretor3) {
+        this.corretor3 = corretor3;
+    }
+
+    public double getComissao1() {
+        if (corretor1<=30000) {
+            comissao1 = 0.075*corretor1;
+        }else if(corretor1<=50000){
+            comissao1 = 0.095*corretor1;
+        }else{
+            comissao1 = 0.125*corretor1;
+        }
+        return comissao1;
+    }
+
+    public void setComissao1(double comissao1) {
+        this.comissao1 = comissao1;
+    }
+
+    public double getComissao2() {
+        if (corretor2<=30000) {
+            comissao2 = 0.075*corretor2;
+        }else if(corretor1<=50000){
+            comissao2 = 0.095*corretor2;
+        }else{
+            comissao2 = 0.125*corretor2;
+        }
+        return comissao2;
+    }
+
+    public void setComissao2(double comissao2) {
+        this.comissao2 = comissao2;
+    }
+
+    public double getComissao3() {
+        if (corretor3<=30000) {
+            comissao3 = 0.075*corretor3;
+        }else if(corretor1<=50000){
+            comissao3 = 0.095*corretor3;
+        }else{
+            comissao3 = 0.125*corretor3;
+        }
+        return comissao3;
+    }
+
+    public void setComissao3(double comissao3) {
+        this.comissao3 = comissao3;
     }
 
    
